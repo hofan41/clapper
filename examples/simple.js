cf5e1f1057d1ce874f4254f46266c1a724596ea5
Ho-Fan Kang <me@hofan41.com>
2015-07-09 17:38:57 -0700

    Add simple example

---
+++ examples/simple.js
@@ -0,0 +1,62 @@
+var FACEBOOK_APP_ID = 'INSERT HERE';
+var FACEBOOK_APP_SECRET = 'INSERT HERE';
+
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+
+server.connection({ port: 8080 });
+
+// Register clapper with the server.
+// This example adds facebook as one of the login providers.
+server.register({ 
+    register: require('../'),
+    options: {                      // plugin options are mandatory
+      defaultRights: {
+        anonymous: { canHazCheezburger: false },
+        authenticated: { canHazCheezburger: true }
+      },
+      cookie: {                     // hapi-auth-cookie strategy options here
+        password: 'TESTING',
+        isSecure: false
+      },
+      logins: [ {
+          displayName: 'Facebook',
+          routeName: 'facebook',    // clapper will create a route at '/auth/facebook'
+          bellProvider: {           // related bell strategy options here
+            provider: 'facebook',
+            password: 'TESTING',
+            clientId: FACEBOOK_APP_ID,
+            clientSecret: FACEBOOK_APP_SECRET,
+            isSecure: false,
+            forceHttps: false
+          }
+      } ]
+    }
+  },
+function (err) {
+    console.log('got here!');
+  // Declare a route that requires the user to have the 'canHazCheezburger' right.
+  // If the user does not have the right, they will be redirected to the error view.
+  // If the user does have the right, the route handler function will be executed.
+  server.route({
+    path: '/',
+    method: 'GET',
+    config: {
+      plugins: {
+        clapper: {
+          canHazCheezburger: true
+        }
+      }
+    },
+    handler: function (request, reply) {
+      return reply('I can haz cheezburger!');
+    }
+  });
+
+  // Start the server.
+  // To test, please access:
+  // 1. http://localhost:8080/              - confirm error page.
+  // 2. http://localhost:8080/auth/facebook - login.
+  // 3. http://localhost:8080/              - confirm cheezburger.
+  server.start();
+});
